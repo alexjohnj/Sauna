2c359f2ccaf5eb50ce4019609b9d4ed28a6277f0
Alex Jackson <alex@alexj.org>
2020-06-04 13:22:03 +0100

    Initial Commit

---
+++ Steam Snooper/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Steam Snooper
+//
+//  Created by Alex Jackson on 04/06/2020.
+//  Copyright © 2020 Alex Jackson. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
