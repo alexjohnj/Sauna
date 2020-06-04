7eb1a9f57464eb41f807df96f56db44702cfdcb5
Alex Jackson <alex@alexj.org>
2020-06-04 13:57:14 +0100

    Add response structure for retrieving the friends list

---
+++ Steam Snooper/API/SteamID.swift
@@ -0,0 +1,11 @@
+//
+//  File.swift
+//  Steam Snooper
+//
+//  Created by Alex Jackson on 04/06/2020.
+//  Copyright © 2020 Alex Jackson. All rights reserved.
+//
+
+struct SteamID: Hashable, RawRepresentable, Codable {
+    let rawValue: String
+}
---
+++ Steam Snooper/AppState.swift
@@ -0,0 +1,9 @@
+//
+//  AppState.swift
+//  Steam Snooper
+//
+//  Created by Alex Jackson on 04/06/2020.
+//  Copyright © 2020 Alex Jackson. All rights reserved.
+//
+
+import Foundation
