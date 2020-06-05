8cb82f9624fd3e59f93b73fb8ad6a76864192073
Alex Jackson <alex@alexj.org>
2020-06-05 13:17:15 +0100

    Configure friend list cells status text and badge

--- Steam Snooper/AppKit Friends List/FriendTableViewCell.swift
+++ Steam Snooper/AppKit Friends List/FriendTableViewCell.swift
@@ -30,7 +30,49 @@ final class FriendTableViewCell: NSTableCellView {
 
     private func configure(with profile: Profile) {
         nameLabel.stringValue = profile.name
-        statusLabel.stringValue = profile.currentGame ?? ""
-        statusLabel.isHidden = profile.currentGame == nil
+        statusImageView.image = statusImage(for: profile)
+        if let statusText = self.statusText(for: profile) {
+            statusLabel.stringValue = statusText
+            statusLabel.isHidden = false
+        } else {
+            statusLabel.stringValue = ""
+            statusLabel.isHidden = true
+        }
+    }
+
+    private func statusText(for profile: Profile) -> String? {
+        if let currentGame = profile.currentGame {
+            return "Playing \(currentGame)"
+        }
+
+        switch profile.status {
+        case .away:
+            return "Away"
+        case .busy:
+            return "Busy"
+        case .lookingToTrade:
+            return "Looking to Trade"
+        case .lookingToPlay:
+            return "Looking to Play"
+        default:
+            return nil
+        }
+    }
+
+    private func statusImage(for profile: Profile) -> NSImage {
+        switch profile.status {
+        case .online,
+             .lookingToPlay,
+             .lookingToTrade:
+            return NSImage(named: NSImage.statusAvailableName)!
+
+        case .snooze,
+             .busy,
+             .away:
+            return NSImage(named: NSImage.statusPartiallyAvailableName)!
+
+        case .offline:
+            return NSImage(named: NSImage.statusUnavailableName)!
+        }
     }
 }
