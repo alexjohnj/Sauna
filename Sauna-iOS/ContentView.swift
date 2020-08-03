ea920def100bb5735eb0c26e3c4548ef262fb47d
Alex Jackson <alex@alexj.org>
2020-08-03 10:28:21 +0100

    Create iOS app target

---
+++ Sauna-iOS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Sauna-iOS
+//
+//  Created by Alex Jackson on 03/08/2020.
+//  Copyright © 2020 Alex Jackson. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Sauna-iOS/SaunaApp.swift
@@ -0,0 +1,18 @@
+//
+//  Sauna_iOSApp.swift
+//  Sauna-iOS
+//
+//  Created by Alex Jackson on 03/08/2020.
+//  Copyright © 2020 Alex Jackson. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct SaunaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
