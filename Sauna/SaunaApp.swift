ec7a0f376f6608b9c3d72a42a530d6b03e4ec676
Alex Jackson <alex@alexj.org>
2021-05-30 17:43:30 +0100

    Create App conforming type

---
+++ Sauna/SaunaApp.swift
@@ -0,0 +1,16 @@
+//
+// Created by Alex Jackson on 30/05/2021.
+// Copyright (c) 2021 Alex Jackson. All rights reserved.
+//
+
+import Foundation
+import SwiftUI
+
+@main
+struct SaunaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            Text("Hello, world!")
+        }
+    }
+}
